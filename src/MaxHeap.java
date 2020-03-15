b7105204eeeb28ff0401512571cda117ee536a5e
MyMasterArtoria <MyMasterArtoria@192.168.0.6>
2020-03-15 16:42:48 -0700

    finished

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -15,12 +15,12 @@ public class MaxHeap implements Heap {
     	while(i<data.length)
     	{
     		this.data[i]=data[i];
-    		swap(i);
+    		swaplogN(i);
     		i++;
     		size++;
     	}
     }
-    private void swap(int i)
+    private void swaplogN(int i)
     {
     	int parent=(i-1)/2;
     	if(this.data[i]>this.data[parent])
@@ -28,35 +28,69 @@ public class MaxHeap implements Heap {
     		int swap=this.data[parent];
     		this.data[parent]=this.data[i];
     		this.data[i]=swap;
-    		swap(parent);
+    		swaplogN(parent);
     	}
     	else 
     	{return;}
     }
    
-    
-   
     // build a heap based on data
     // to be implemented in O(n)
     public void MaxHeapN(Integer[] data) {
-      
+    	int n=data.length;
+    	for(int i=0;i<n;i++) {
+    		this.data[i]=data[i];
+    	}
+    	size=n;
+      int lastnode=(n/2)-1;
+      	for(int j=lastnode;j>=0;j--) {
+      		heapify(this.data,n,j);
+      	}
     }
+    private void heapify(Integer arr[], int n, int i) {
+    	int largest = i; 
+        int l = 2 * i + 1; 
+        int r = 2 * i + 2;
+        if (l < n && arr[l] > arr[largest]) 
+        { largest = l;} 
+        if (r < n && arr[r] > arr[largest]) 
+            {largest = r;} 
+        if (largest != i) { 
+        	{
+        		int swap=arr[i];
+        		arr[i]=arr[largest];
+        		arr[largest]=swap;
+        	} 
 
+            heapify(arr, n, largest); 
+        } 
+    }
+    
     // add an item to the heap
     public boolean add(Integer item) {
-      
-        return false;
+    	this.data[size]=item;
+    	swaplogN(size);
+    	size++;
+        return true;
     }
 
     // return the max item in the heap
     public Integer get() {
    
-        return null;
+        return this.data[0];
     }
 
     // remove the root item
     public Integer pop() {
-      
-        return null;
+    	int n=data.length;
+        int lastElement = data[n - 1];  
+        int root=data[0];
+        data[0] = lastElement;  
+        size= size - 1; 
+        n=n-1;
+        heapify(data,n, 0); 
+        data[size]=null;
+        return root; 
+        
     }
 }
--- src/MaxHeapTest.java
+++ src/MaxHeapTest.java
@@ -1,16 +1,52 @@
 import static org.junit.Assert.*;
-
+import org.junit.Test; 
+import java.util.Arrays;
 public class MaxHeapTest {
+	@Test
 	public void LogNtest() {
 	Integer[] test1= {3,1,5,2,6,4};
 	MaxHeap H=new MaxHeap(6);
 	H.MaxHeapLogN(test1);
-	for(int i=0;i<H.data.length;i++)
-	{
-	System.out.println(H.data[i]);
-	}
 	Integer[] expect={6,5,4,1,2,3};
+	
 	assertArrayEquals(H.data,expect);
 	}
 	
+	@Test
+	public void nTest(){
+		Integer[] test1= {3,1,5,2,6,4};
+		MaxHeap H=new MaxHeap(6);
+		H.MaxHeapN(test1);
+		Integer[] expect={6,3,5,2,1,4};
+		assertArrayEquals(H.data,expect);
+	}
+	@Test
+	public void addTest() {
+		Integer[] test1= {8,7,6,4,3,5,2,1};
+		MaxHeap H=new MaxHeap(9);
+		H.MaxHeapLogN(test1);
+		H.add(9);
+		Integer[] expect={9,8,6,7,3,5,2,1,4};
+		assertArrayEquals(H.data,expect);
+	}
+	@Test
+	public void getTest() {
+		Integer[] test1= {8,7,6,4,3,5,2,1};
+		MaxHeap H=new MaxHeap(8);
+		H.MaxHeapLogN(test1);
+		Integer expect=8;
+		Integer test=H.get();
+		assertEquals(test,expect);
+		
+	}
+	@Test
+	public void popTest() {
+		Integer[] test1= {9,8,6,7,3,5,2,1,4};
+		MaxHeap H=new MaxHeap(9);
+		H.MaxHeapLogN(test1);
+		Integer test=H.pop();
+		Integer expect=9;
+		System.out.println(Arrays.toString(H.data));
+		assertEquals(test,expect);
+	}
 }
